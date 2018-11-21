9d575d454b067df39374e9ed5217bcb5605c24ca
Nikke Leskelä <nileskel@gmail.com>
2018-11-21 20:05:41 +0000

    Update templates.

---
+++ templates/scripts/devrun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd $SCRIPT_DIR
+cd ..
+
+# run
+NET_NAME=saavunet ./s_node_1st.sh yarn run devrun
---
+++ templates/scripts/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd $SCRIPT_DIR
+cd ..
+
+./s_node_1st.sh yarn run test ${@:1}
