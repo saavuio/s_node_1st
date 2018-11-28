24142815128b86906f6dcb55b59dda5f006c39ff
Nikke Leskelä <nileskel@gmail.com>
2018-11-28 12:46:46 +0000

    Fix eject.sh script template.

--- templates/scripts/eject.sh
+++ templates/scripts/eject.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd $SCRIPT_DIR
+cd ..
+
 echo "Fetching stuff from inside the container."
 
 IMAGE_NAME=s_node_1st
-RUNNER=./$IMAGE_NAME.sh
+RUNNER=./${IMAGE_NAME}.sh
 if [ ! -f $RUNNER ]; then exit 1; fi
 CONTAINER_NAME=${IMAGE_NAME}_tmp
 CONTAINER_NAME=$CONTAINER_NAME NO_TTY=1 $RUNNER eval "sleep 30;" &
 
 sleep 3
 
-docker cp $CONTAINER_NAME:/$IMAGE_NAME/node_modules ./
-docker cp $CONTAINER_NAME:/$IMAGE_NAME/.eslintrc.js ./
+docker cp ${CONTAINER_NAME}:/$IMAGE_NAME/node_modules ./
+docker cp ${CONTAINER_NAME}:/$IMAGE_NAME/.eslintrc.js ./
