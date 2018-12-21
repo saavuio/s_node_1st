f3fdb266fcb9c292694699ab6726577247e103a2
Nikke Leskelä <nileskel@gmail.com>
2018-12-21 17:31:55 +0000

    Update paths.

--- scripts/after_get.sh
+++ scripts/after_get.sh
@@ -9,19 +9,19 @@ mkdir ${PROJECT_ROOT_PATH}/src
 mkdir ${PROJECT_ROOT_PATH}/dist
 
 # link to self
-cp ./s_node_1st/templates/s_node_1st.sh ${PROJECT_ROOT_PATH}
+cp ./s_node_1st/base/templates/s_node_1st.sh ${PROJECT_ROOT_PATH}
 
 # scripts
 if [ ! -d ${PROJECT_ROOT_PATH}/scripts ]; then
-  cp -a ./s_node_1st/templates/scripts ${PROJECT_ROOT_PATH}/scripts
+  cp -a ./s_node_1st/base/templates/scripts ${PROJECT_ROOT_PATH}/scripts
 fi
 # .gitignore skel
 if [ ! -f ${PROJECT_ROOT_PATH}/.gitignore ]; then
-  cp ./s_node_1st/templates/gitignore ${PROJECT_ROOT_PATH}/.gitignore
+  cp ./s_node_1st/base/templates/gitignore ${PROJECT_ROOT_PATH}/.gitignore
 fi
 # env-development skel
 if [ ! -f ${PROJECT_ROOT_PATH}/env-development ]; then
-  cp ./s_node_1st/templates/env-development ${PROJECT_ROOT_PATH}/env-development
+  cp ./s_node_1st/base/templates/env-development ${PROJECT_ROOT_PATH}/env-development
 fi
 # package.json skel
 if [ ! -f ${PROJECT_ROOT_PATH}/package.json ]; then
